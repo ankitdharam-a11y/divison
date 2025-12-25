21692201ef541fcd35ac080bf93c8f415f449cbf
ankitdharam-a11y <ankitdharam@gmail.com>
2025-12-24 23:32:42 -0800

    Add files via upload

---
+++ d.py
@@ -0,0 +1,8 @@
+print("enter a number(numerator):")
+num=int(input())
+print("enter a number(denomenator):")
+number=int(input())
+if num%number==0:
+    print("number is divisible")
+else:
+    print("number is not divisible")
